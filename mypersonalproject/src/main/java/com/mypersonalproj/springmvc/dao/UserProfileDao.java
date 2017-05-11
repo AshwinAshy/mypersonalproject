c66f2f8dfe4f83736b12d4a94250426c891f399d
Ashwin <ashwinaradhya@gmail.com>
2017-05-12 00:27:34 +0530

    Initial Commit To LocalRepository.

---
+++ mypersonalproject/src/main/java/com/mypersonalproj/springmvc/dao/UserProfileDao.java
@@ -0,0 +1,15 @@
+package com.mypersonalproj.springmvc.dao;
+
+import java.util.List;
+
+import com.mypersonalproj.springmvc.model.UserProfile;
+
+
+public interface UserProfileDao {
+
+	List<UserProfile> findAll();
+	
+	UserProfile findByType(String type);
+	
+	UserProfile findById(int id);
+}
---
+++ src/main/java/com/myspringproj/controllers/DefaultController.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.myspringproj.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+ 
+/**
+ *
+ * @author john
+ */
+@Controller
+public class DefaultController {
+    
+   @RequestMapping(value = "/", method = RequestMethod.GET)
+   public String index(ModelMap map) {
+       System.err.println("Inside the controller.......................");
+       map.put("msg", "Hello Spring 4 Web MVC!");
+       return "index";
+   }
+    
+}
